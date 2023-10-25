8283928718ec3296655c41a5b2b29d80e502e8e2
Juangamis <145427094+Juangamis@users.noreply.github.com>
2023-10-25 23:15:24 +0800

    Add files via upload

---
+++ exercise lecture 20/component/About.js
@@ -0,0 +1,17 @@
+// about page
+function About(){
+    return (
+        <div>
+            <h2> Front-end web development is the development of the graphical 
+                user interfacxe of a website, through the use of HTML, CSS, and
+                JavaScript, so that users can view and interact with that website
+            .</h2>
+
+            Read more the article:
+            <a href=
+            "https://forntendmasters.com/guides/front-end-handbook/2019/">
+                https://frontendmasters.com/guides/front-end-handbook/2019/
+            </a>
+        </div>
+    );
+}
